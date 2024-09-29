cd3839a3086504909a8a0399d7a009f7bd2699be
NiclasssLua <niclas.frey@gmx.de>
2024-09-29 05:06:30 +0200

    added source-map for debugging

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { API } from 'homebridge';
 import { PLATFORM_NAME } from './settings';
 import { TuyaLaundryNotifyPlatform } from './platform';
 
+import 'source-map-support/register';
+
 /**
  * This method registers the platform with Homebridge
  */
