6d2ef757fd092dd5498c1246694e91e8c977c3b4
Niclas <78554432+Niclassslua@users.noreply.github.com>
2025-08-28 15:28:52 +0100

    Share discovery results across device trackers

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'test/platform.test.ts',
+    'test/lib/pushGateway.test.ts',
+    'test/lib/laundryDevice.test.ts',
+    'test/lib/laundryDeviceTracker.test.ts',
+  ],
 };
